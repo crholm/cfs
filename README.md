c0423bc6b9b757a7f9ab89053104bc6c04085fd3
Rasmus Holm <rasmus.holm@modularfinance.se>
2020-01-22 09:34:59 +0100

    final touches

--- README.md
+++ README.md
@@ -1,16 +1,36 @@
 
-```bash
+Instructions
+* Use a linux dist, preferable ubuntu
+* Install go, https://golang.org/doc/install
 
+Creating a rootfs
+```bash
 $ sudo apt install debootstrap 
 $ mkdir rootfs
 $ sudo debootstrap eoan rootfs/
+```
 
+Build allocate
+```bash
+$ cd utils
+$ go build allocate.go
+$ cp ./allocate ../rootfs
+```
 
+Syscall informaiton
+```bash
 $ man 2 clone
 $ man 2 unshare
-
-$ echo $$
-
-go run -exec sudo main.go run --env "hello=container" --memory 50000000 bash
-
 ```
+
+Running the "container"
+```bash
+$ go run -exec sudo main.go run --env "hello=container" --memory 50000000 bash 
+$ echo $$    ## shows current pid
+$ ps 
+$ ps aux
+$ echo $hello
+$ env
+$ ./allocate 100
+```
+
--- main.go
+++ main.go
@@ -8,7 +8,6 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
-	"strconv"
 	"strings"
 	"syscall"
 )
@@ -36,13 +35,26 @@ func main() {
 func run(args ...string) {
 
 	fmt.Println("Running", args, os.Getpid())
+
+	// Executing the same binary again, but replacing run with child in order to utilize the new namespaces
 	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, args...)...)
 	cmd.Stdout = os.Stdout
 	cmd.Stdin = os.Stdin
 	cmd.Stderr = os.Stderr
 	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS,
-		Unshareflags: syscall.CLONE_NEWNS,
+
+		Cloneflags:
+		//  Creates a new namespace  ($ man 2 clone)
+		//  for hostname and more
+			syscall.CLONE_NEWUTS |
+
+		//  Creates a new namespace  ($ man 2 clone)
+		//  for process ids
+			syscall.CLONE_NEWPID |
+
+		//  Creates a new namespace  ($ man 2 clone)
+		//  for mounting things
+			syscall.CLONE_NEWNS,
 	}
 	check(cmd.Run())
 }
@@ -83,24 +95,36 @@ func child(args ...string) {
 	cmd.Stdin = os.Stdin
 	cmd.Stderr = os.Stderr
 
+	// Sets the hostname of in the New UTS for the process
 	check(syscall.Sethostname([]byte(fmt.Sprintf("cfs-%d", rand.Int31n(10000)))))
+
+	// Sets the root filesystem to the dir rootfs
 	check(syscall.Chroot("rootfs"))
+
+	// Sets the dir in which the process starts from
 	check(syscall.Chdir("/"))
+
+	// Mounts a news proc folder in order to gain access to process information within the container
 	check(syscall.Mount("proc", "proc", "proc", 0, ""))
 	defer check(syscall.Unmount("proc", 0))
 
 	check(cmd.Run())
 }
 
+// Set the memory cgroup for the process given the number of bytes
 func cgMem(maxMemSize int) {
 	size := fmt.Sprintf("%d", maxMemSize)
 
-	cgroups := "/sys/fs/cgroup/"
-	memory := filepath.Join(cgroups, "memory")
-	_ = os.Mkdir(filepath.Join(memory, "cfs"), 0755)
-	check(ioutil.WriteFile(filepath.Join(memory, "cfs/memory.swappiness"), []byte("0"), 0700))
-	check(ioutil.WriteFile(filepath.Join(memory, "cfs/memory.limit_in_bytes"), []byte(size), 0700))
+	memory := "/sys/fs/cgroup/memory/cfs"
+	// Creates the cgroup dir if it does not exist
+	_ = os.Mkdir(memory, 0755)
 
-	// Writing the pid on which to apply the cgroup to
-	check(ioutil.WriteFile(filepath.Join(memory, "cfs/cgroup.procs"), []byte(strconv.Itoa(os.Getpid())), 0700))
+	// Disables swap for the container
+	check(ioutil.WriteFile(filepath.Join(memory, "memory.swappiness"), []byte("0"), 0700))
+
+	// Writes the maximum amount of memory allowed to be used
+	check(ioutil.WriteFile(filepath.Join(memory, "memory.limit_in_bytes"), []byte(size), 0700))
+
+	// Writing the pid of the current process to apply the cgroup to it
+	check(ioutil.WriteFile(filepath.Join(memory, "cgroup.procs"), []byte(fmt.Sprintf("%d", os.Getpid())), 0700))
 }
